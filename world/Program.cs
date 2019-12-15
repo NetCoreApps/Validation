5f476a4786f3fd4cb06d0cc841970218357239c6
Demis Bellot <demis.bellot@gmail.com>
2019-12-15 17:56:14 -0500

    Upgrade to .NET Core 3.1

--- world/Program.cs
+++ world/Program.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using ServiceStack;
 
 namespace Validation
 {
@@ -19,6 +20,6 @@ namespace Validation
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+                .UseModularStartup<Startup>();
     }
 }
--- world/Startup.cs
+++ world/Startup.cs
@@ -12,14 +12,11 @@ using ServiceStack;
 
 namespace Validation
 {
-    public class Startup
+    public class Startup : ModularStartup
     {
-        public IConfiguration Configuration { get; }
-        public Startup(IConfiguration configuration) => Configuration = configuration;
-
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
-        public void ConfigureServices(IServiceCollection services)
+        public new void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
         }
